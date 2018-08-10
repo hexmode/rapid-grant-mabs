71aa2612089d464ef047ea9d5a00093f3a4e6ab1
Mark A. Hershberger <mah@nospam.nichework.com>
2018-08-10 19:47:15 -0400

    caps

--- setupmabs.sh
+++ setupmabs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 ssh -o UserKnownHostsFile=hostpubkey mabs.default 'mkdir -p /home/mah/repo; chmod 1777 /home/mah/repo'
-grep -q MABSrepo.*= LocalSettings.php || echo '$MABSrepo = "/home/mah/repo";' | tee -a LocalSettings.php
+grep -q MABSRepo.*= LocalSettings.php || echo '$MABSRepo = "/home/mah/repo";' | tee -a LocalSettings.php
