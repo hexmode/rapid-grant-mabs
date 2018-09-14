1b928fa319d9ece6319876e5b613515f0f3f9859
Mark A. Hershberger <mah@nichework.com>
2018-09-14 16:51:18 -0400

    Sync with emacs cruft

--- setupenv.sh
+++ setupenv.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
-test -d /home/mah/client/mabs/mediawiki || git clone /home/mah/work/code/mediawiki/core /home/mah/client/mabs/mediawiki
-cd /home/mah/client/mabs/mediawiki
-git remote set-url origin https://gerrit.wikimedia.org/r/mediawiki/core.git
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+. .envrc
+
+test -d ${MW_INSTALL_DIR} || git clone ${LOCAL_GIT} ${MW_INSTALL_DIR}
+cd ${MW_INSTALL_DIR}
+git remote set-url origin ${UPSTREAM_GIT}
 git fetch
 branch=`git branch -q | awk '/^\*/ {print $2}'`
-test "$branch" = "REL1_31" || git checkout REL1_31
+test "$branch" = "${RELBRANCH}" || git checkout ${RELBRANCH}
 git submodule update --init
-echo on REL1_31
-cd /home/mah/client/mabs
+echo on ${RELBRANCH}
+cd $DIR
 
-grep -q "/home/mah/client/mabs" /etc/exports || echo "/home/mah/client/mabs *(rw,root_squash,subtree_check)" | sudo tee -a /etc/exports
+grep -q "${DIR}" /etc/exports || echo "${DIR} *(rw,root_squash,subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -ra
--- sync-site.el
+++ sync-site.el
@@ -8,13 +8,18 @@
 ;;; Code:
 
 (use-package auto-shell-command)
-(let ((this-dir (file-name-directory (buffer-file-name))))
-  (ascmd:add
-   (list this-dir
-		 (concat "rsync -av --delete --exclude .git --exclude /mediawiki/images "
-				 "--exclude /logs --exclude '*~' --exclude '#*#' "
-				 this-dir " "
-				 (concat (getenv "HOST") ":" (getenv "DIR") )))))
+(let ((host (getenv "HOST"))
+	  (dir (getenv "DIR"))
+	  (this-dir (file-name-directory (buffer-file-name))))
+  (when
+	  (and (and host dir)
+		   (not (assoc this-dir ascmd:setting)))
+	(ascmd:add
+	 (list this-dir
+		   (concat "rsync -av --delete --exclude .git --exclude /mediawiki/images "
+				   "--exclude /logs --exclude '*~' --exclude '#*#' "
+				   this-dir " "
+				   (concat host ":" dir))))))
 
-(provide 'sync-up)
-;;; sync-up ends here
+(provide 'sync-site)
+;;; sync-site ends here
